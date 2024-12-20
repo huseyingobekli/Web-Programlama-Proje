e46d77cf3d7a28bc94e6fb5c30a2960bfc9154c1
huseyin <huseyingobekli29@gmail.com>
2024-12-20 23:53:58 +0300

    temp dosyasi olusturuldu edit sayfasi eklendi

--- Web-Programlama-Proje/Controllers/HomeController.cs
+++ Web-Programlama-Proje/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Web_Programlama_Proje.Controllers
             return View();
         }
 
+        public IActionResult test3()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
