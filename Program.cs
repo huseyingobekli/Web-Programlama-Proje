b5f939e201f1badf30d952e13392c8a2be55cfdc
Okan <121483995+Okanbasol@users.noreply.github.com>
2024-12-23 22:20:51 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,23 +6,28 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // HTTP istek hattýný yapýlandýr
-if (!app.Environment.IsDevelopment())
-{
-    // Hata sayfasý ve HSTS kullan
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection(); // HTTPS yönlendirme
-app.UseStaticFiles(); // Statik dosyalar
-
-app.UseRouting(); // Yönlendirme
-
-app.UseAuthorization(); // Yetkilendirme
-
-// Varsayýlan rota
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+ConfigureApp(app);
 
 app.Run(); // Uygulamayý çalýþtýr
+
+void ConfigureApp(WebApplication app)
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        // Hata sayfasý ve HSTS kullan
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    // Ortak middleware'ler
+    app.UseHttpsRedirection(); // HTTPS yönlendirme
+    app.UseStaticFiles(); // Statik dosyalar
+
+    app.UseRouting(); // Yönlendirme
+    app.UseAuthorization(); // Yetkilendirme
+
+    // Varsayýlan rota
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+}
