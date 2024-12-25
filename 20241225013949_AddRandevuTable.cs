1ab5229be5a64545c4f3c55b7e870ea8dca005d8
Okan <121483995+Okanbasol@users.noreply.github.com>
2024-12-25 22:58:47 +0300

    Add files via upload

---
+++ 20241225013949_AddRandevuTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KuaforSistemi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRandevuTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
